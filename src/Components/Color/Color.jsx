601181e29ea564a99f90691bf36e3624f7f63b08
Jose Reyes <jose_reyes@outlook.de>
2026-07-22 18:02:59 +0200

    Delete a color from the theme: commit 1

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,12 +1,23 @@
 import "./Color.css";
+import { useState } from "react";
 
-export default function Color({ color }) {
+export default function Color({ color, handleDeleteColor }) {
+  const [isConfirming, setIsConfirming] = useState();
   return (
     <>
       <div className="color-card" style={{ color: color.contrastText, backgroundColor: color.hex }}>
         <p className="color-card-headline">{color.hex}</p>
         <p>{color.role}</p>
         <p>{color.contrastText}</p>
+        {isConfirming ? (
+          <p>
+            Ar you sure?
+            <button onClick={() => handleDeleteColor(color.id)}>Yes</button>
+            <button onClick={() => setIsConfirming(false)}>Cancel</button>
+          </p>
+        ) : (
+          <button onClick={() => setIsConfirming(true)}>DELETE</button>
+        )}
       </div>
     </>
   );
