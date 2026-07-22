249c253d110abe059f0e1e4ff446fa693f7aec54
Jose Reyes <jose_reyes@outlook.de>
2026-07-22 13:40:52 +0200

    Add a Color to the Theme: commit 3

--- src/ColorForm/ColorForm.jsx
+++ src/ColorForm/ColorForm.jsx
@@ -11,21 +11,32 @@ export default function ColorForm({ onAdd }) {
   return (
     <>
       <form onSubmit={onSubmit} className="color-form">
-        <div className="color-form__fields">
+        <div>
           <div className="color-form__field">
-            <label htmlFor="role">Role</label>
-            <input type="text" name="role" id="role" />
+            <label htmlFor="role" className="color-form__labels">
+              Role
+            </label>
+            <input type="text" name="role" id="role" defaultValue="latest color added" />
           </div>
           <div className="color-form__field">
-            <label htmlFor="hex">HEX</label>
-            <ColorInput id="hex" name="hex" />
+            <label htmlFor="hex" className="color-form__labels">
+              HEX
+            </label>
+            <ColorInput id="hex" name="hex" defaultValue="pick a color" defaultColor={"#000000"} />
           </div>
           <div className="color-form__field">
-            <label htmlFor="contrastText">Contrast Text</label>
-            <ColorInput id="contrastText" name="contrastText" />
+            <label htmlFor="contrastText" className="color-form__labels">
+              Contrast Text
+            </label>
+            <ColorInput
+              id="contrastText"
+              name="contrastText"
+              defaultValue="pick a color"
+              defaultColor={"#ffffff"}
+            />
           </div>
           <div className="color-form__button-container">
-            <button class="color-form__button-add" buttontype="submit">
+            <button className="color-form__button-add" type="submit">
               ADD COLOR
             </button>
           </div>
