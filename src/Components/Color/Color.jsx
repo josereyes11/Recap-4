1e0be1a201261f1c5b2bf4c5a0d21e0b7228202e
Jose Reyes <jose_reyes@outlook.de>
2026-07-23 03:46:41 +0200

    Edit a color from the theme: commit 5 extra styling

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,8 @@
 import "./Color.css";
 import { useState } from "react";
 import ColorForm from "../../ColorForm/ColorForm";
+import deleteIcon from "../../assets/delete-icon.svg";
+import editIcon from "../../assets/edit-icon.svg";
 
 export default function Color({ color, handleDeleteColor, handleEditColor }) {
   const [isConfirming, setIsConfirming] = useState();
@@ -11,34 +13,40 @@ export default function Color({ color, handleDeleteColor, handleEditColor }) {
         <p className="color-card-headline">{color.hex}</p>
         <p>{color.role}</p>
         <p>{color.contrastText}</p>
-        {isConfirming ? (
-          <div>
-            <p className="color-card-question">Are you sure?</p>
-            <button onClick={() => handleDeleteColor(color.id)}>Yes</button>
-            <button onClick={() => setIsConfirming(false)}>Cancel</button>
-          </div>
-        ) : (
-          <button onClick={() => setIsConfirming(true)}>DELETE</button>
-        )}
-        {isEditing ? (
-          <div>
-            <div className="color-card-cancel-container">
-              <button className="color-card-cancel-button" onClick={() => setIsEditing(false)}>
-                Cancel
-              </button>
+        <div className="card-icon-buttons">
+          {isConfirming ? (
+            <div className="color-card-question-container">
+              <p className="color-card-question">Are you sure?</p>
+              <button onClick={() => handleDeleteColor(color.id)}>Yes</button>
+              <button onClick={() => setIsConfirming(false)}>Cancel</button>
             </div>
-            <ColorForm
-              buttonLabel="UPDATE COLOR"
-              initialData={color}
-              onAdd={(data) => {
-                handleEditColor(color.id, data);
-                setIsEditing(false);
-              }}
-            />
-          </div>
-        ) : (
-          <button onClick={() => setIsEditing(true)}>EDIT</button>
-        )}
+          ) : (
+            <button className="icon-button" onClick={() => setIsConfirming(true)}>
+              <img src={deleteIcon} alt="Delete" />
+            </button>
+          )}
+          {isEditing ? (
+            <div className="isEditing-container">
+              <div className="color-card-cancel-container">
+                <button className="color-card-cancel-button" onClick={() => setIsEditing(false)}>
+                  CANCEL
+                </button>
+              </div>
+              <ColorForm
+                buttonLabel="UPDATE COLOR"
+                initialData={color}
+                onAdd={(data) => {
+                  handleEditColor(color.id, data);
+                  setIsEditing(false);
+                }}
+              />
+            </div>
+          ) : (
+            <button className="icon-button" onClick={() => setIsEditing(true)}>
+              <img src={editIcon} alt="Edit" />
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
